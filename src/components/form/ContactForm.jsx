a9cb4f6b255db84ecc581ae78cdfb52ba2c797f0
Your NameMoCheeseKy <rifkyking04@gmail.com>
2025-06-19 01:15:45 +0700

    add section of  project

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -24,11 +24,11 @@ const ContactForm = () => {
   return (
     <div className='w-full md:max-w-[488px] h-fit p-6 font-mono text-white bg-[#252525] rounded-lg shadow-xl min-h-[400px] border-[1px] border-gray-600 '>
       <div className='mb-4'>
-        <div className='text-green whitespace-normal break-words'>
+        <div className='break-words whitespace-normal text-green'>
           Please drop your email, name, <br />
           and tell me how i can help you ^^
         </div>
-        <div className='text-green whitespace-normal break-words mt-4'>
+        <div className='mt-4 break-words whitespace-normal text-green'>
           ----------
         </div>
         {step >= 1 && (
@@ -38,20 +38,19 @@ const ContactForm = () => {
               <span className='text-blue'>email?</span>
             </p>
             {email ? (
-              <div className='text-green whitespace-normal break-words'>
+              <div className='break-words whitespace-normal text-green'>
                 ✔ {email}
               </div>
             ) : (
               <input
                 type='email'
-                className='w-full text-green bg-transparent border-b border-blue focus:outline-none'
+                className='w-full bg-transparent border-b text-green border-blue focus:outline-none'
                 onKeyDown={(e) => {
                   if (e.key === 'Enter') {
                     setEmail(e.target.value);
                     handleNext();
                   }
                 }}
-                autoFocus
               />
             )}
           </>
@@ -63,13 +62,13 @@ const ContactForm = () => {
               Awesome! And whats your <span className='text-blue'>name?</span>
             </p>
             {name ? (
-              <div className='text-green whitespace-normal break-words'>
+              <div className='break-words whitespace-normal text-green'>
                 ✔ {name}
               </div>
             ) : (
               <input
                 type='text'
-                className='w-full text-green bg-transparent border-b border-blue focus:outline-none'
+                className='w-full bg-transparent border-b text-green border-blue focus:outline-none'
                 onKeyDown={(e) => {
                   if (e.key === 'Enter') {
                     setName(e.target.value);
@@ -90,17 +89,17 @@ const ContactForm = () => {
             </p>
             {description ? (
               <>
-                <div className='text-green whitespace-normal break-words'>
+                <div className='break-words whitespace-normal text-green'>
                   ✔ {description}
                 </div>
-                <div className='text-green whitespace-normal break-words mt-4'>
+                <div className='mt-4 break-words whitespace-normal text-green'>
                   ----------
                 </div>
               </>
             ) : (
               <textarea
                 rows='2'
-                className='w-full text-green bg-transparent border-b resize-none border-blue focus:outline-none'
+                className='w-full bg-transparent border-b resize-none text-green border-blue focus:outline-none'
                 onKeyDown={(e) => {
                   if (e.key === 'Enter') {
                     setDescription(e.target.value);
@@ -115,25 +114,25 @@ const ContactForm = () => {
       </div>
       {step >= 4 && (
         <>
-          <p className='text-blue-300 mt-6'>Thank you! Plese check again:</p>
+          <p className='mt-6 text-blue-300'>Thank you! Plese check again:</p>
           <div className='flex flex-col gap-4 mt-4'>
             <div>
-              <div className='text-green whitespace-normal break-words'>
+              <div className='break-words whitespace-normal text-green'>
                 Email:
               </div>
               <div className='text-white'>{email}</div>
             </div>
             <div>
-              <div className='text-green whitespace-normal break-words'>
+              <div className='break-words whitespace-normal text-green'>
                 Name:
               </div>
               <div className='text-white'>{name}</div>
             </div>
             <div>
-              <div className='text-green whitespace-normal break-words'>
+              <div className='break-words whitespace-normal text-green'>
                 Description:
               </div>
-              <div className='text-white whitespace-normal break-words'>
+              <div className='text-white break-words whitespace-normal'>
                 {description}
               </div>
             </div>
@@ -146,10 +145,10 @@ const ContactForm = () => {
               </p>
               {SendStatus ? (
                 <>
-                  <div className='text-green whitespace-normal break-words'>
+                  <div className='break-words whitespace-normal text-green'>
                     ✔ {SendStatus}
                   </div>
-                  <div className='text-green whitespace-normal break-words mt-6'>
+                  <div className='mt-6 break-words whitespace-normal text-green'>
                     Thank you for reaching out! <br />
                     I’ll review your message and get back to you shortly ^^{' '}
                     <br />
@@ -159,7 +158,7 @@ const ContactForm = () => {
               ) : (
                 <input
                   type='sendStatus'
-                  className='w-full text-green bg-transparent border-b border-blue focus:outline-none'
+                  className='w-full bg-transparent border-b text-green border-blue focus:outline-none'
                   onChange={(e) => setSendStatus(e.target.value)}
                   autoFocus
                 />
